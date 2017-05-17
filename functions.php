825dd85891191e0a9aa0e57e6978758fc47e28f1
Andrew Minion <andrew@andrewrminion.com>
2017-05-17 16:06:07 -0400

    fix typo

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@
 function ge_enqueue_minified_assets() {
     wp_dequeue_style( 'theme-style' );
     wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/style.min.css' );
-    wp_enqueue_script( 'booth-pricing', get_stylesheet_directory_uri() . '/js/application-form-pricing-min.js', array( 'jquery' ), NULL, true );
+    wp_enqueue_script( 'booth-pricing', get_stylesheet_directory_uri() . '/js/application-form-pricing.min.js', array( 'jquery' ), NULL, true );
 }
 add_action( 'wp_enqueue_scripts', 'ge_enqueue_minified_assets' );
