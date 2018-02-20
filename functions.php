febfa4e71308f04c05f23409e8d358edee387f96
Andrew Minion <andrew@andrewrminion.com>
2018-02-20 16:25:53 -0500

    add cache-busting theme version

--- functions.php
+++ functions.php
@@ -5,6 +5,6 @@
  */
 function ge_enqueue_minified_assets() {
     wp_dequeue_style( 'theme-style' );
-    wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/style.min.css' );
+    wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/style.min.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'ge_enqueue_minified_assets' );
