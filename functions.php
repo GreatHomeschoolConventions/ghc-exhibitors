973279a3bea333085aefce0664b9ffb29dfc724b
Andrew Minion <andrew@andrewrminion.com>
2018-05-03 10:12:41 -0400

    add PHPCS settings and fix PHPCS and JSLint issues

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
+/**
+ * Theme functions
+ *
+ * @package GHC-Exhibitors
+ */
 
 /**
  * Enqueue assets
  */
 function ge_enqueue_minified_assets() {
-    wp_dequeue_style( 'theme-style' );
-    wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/style.min.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_dequeue_style( 'theme-style' );
+	wp_enqueue_style( 'theme', get_stylesheet_directory_uri() . '/style.min.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 add_action( 'wp_enqueue_scripts', 'ge_enqueue_minified_assets' );
--- js/application-form-pricing.js
+++ js/application-form-pricing.js
@@ -1,99 +1,112 @@
-(function($){
-    $(document).ready(function(){
-        // form setup
-        $('.wpcf7-form-control-wrap.locations label[for*="quantity"]').hide();
-        $('input[name="quantity"], input[name="total-amount"], input[name="todays-payment"]').attr('disabled', true);
-        $('span.todays-payment').append('<span class="small"> (automatically calculated)</span>');
+/**
+ * Exhibitor application form pricingGeneral-
+ *
+ * @package GHC-Exhibitors
+ */
 
-        // show/hide individual booth quantities
-        $('input[name="locations[]"]').on('change', function() {
-            if ($(this).is(':checked')) {
-                $(this).parents('.wpcf7-list-item').find('label.quantity').show().find('input').val(1);
-            } else {
-                $(this).parents('.wpcf7-list-item').find('label.quantity').hide().find('input').val(0);
-            }
-        });
+/*global document, jQuery */
 
-        // handle pluralization
-        $('input.quantity-by-convention').on('change', function() {
-            $(this).next('.plural').html($(this).val() != 1 ? 's' : '');
-        });
+(function ($) {
+	"use strict";
 
-        // handle changes
-        $('input.quantity-by-convention, input[name="locations[]"], input[name^="quantity"], input[name="payment-option"]').on('change', updatePaymentInfo);
+	/**
+	 * Show/hide payment fields as necessary
+	 *
+	 * @param {object} input selected or changed radio button
+	 */
+	function handlePaymentFields(input) {
+		if (input.val().indexOf('online via credit card') > -1) {
+			$('.online-payment').slideDown();
+			$('.offline-payment').slideUp();
+		} else {
+			$('.online-payment').slideUp();
+			$('.offline-payment').slideDown();
+		}
+	}
 
-        // main function
-        updatePaymentInfo();
+	/**
+	 * Update payment info
+	 */
+	function updatePaymentInfo() {
+		// Set variables.
+		var pricingGeneral = [525, 500, 475, 450, 450],
+			pricingDiscount = [475, 450, 425, 400, 400],
+			discount = new Date() <= new Date('2017-08-31 23:59:59'),
+			depositDeadline = new Date() <= new Date('2017-12-15 23:59:59'),
+			locationQuantity = $('input[name="locations[]"]:checked').length,
+			boothQuantity = 0,
+			boothPricingIndex = 0,
+			perBoothPrice = 0,
+			todaysPayment = 0;
 
-        // show/hide payment fields
-        $('input[name="payment-method"]').on('change', handlePaymentFields($(this)));
-        handlePaymentFields($('input[name="payment-method"]:checked'));
+		// Hide early-bird stuff if date is past.
+		if (!discount) {
+			$('.early-bird').hide();
+		}
 
-        // re-enable disabled fields prior to form submission
-        $('form.wpcf7-form').on('submit', function(){
-            $('input[disabled]').attr('disabled', false);
-        });
-    });
+		// Check for deposit deadline.
+		if (!depositDeadline) {
+			$('input[name="payment-option"][value*="50%"]').parents('.wpcf7-list-item').remove();
+		}
 
-    /**
-     * Show/hide payment fields as necessary
-     * @param {object} input selected or changed radio button
-     */
-    function handlePaymentFields(input) {
-        if (input.val().indexOf('online via credit card') > -1) {
-            $('.online-payment').slideDown();
-            $('.offline-payment').slideUp();
-        } else {
-            $('.online-payment').slideUp();
-            $('.offline-payment').slideDown();
-        }
-    };
+		// Count up number of total booths.
+		$('.locations input[name*="quantity"]').each(function () {
+			boothQuantity += Number($(this).val());
+		});
 
-    /**
-     * Update payment info
-     */
-    function updatePaymentInfo() {
-        // set variables
-        var pricingGeneral = [525, 500, 475, 450, 450],
-            pricingDiscount = [475, 450, 425, 400, 400],
-            discount = new Date() <= new Date('2017-08-31 23:59:59'),
-            depositDeadline = new Date() <= new Date('2017-12-15 23:59:59'),
-            deposit = $('input[name="payment-option"]').val(),
-            locationQuantity = $('input[name="locations[]"]:checked').length,
-            boothQuantity = 0;
+		// Figure pricing.
+		boothPricingIndex = locationQuantity > 0 ? locationQuantity - 1 : 0;
+		perBoothPrice = discount ? pricingDiscount[boothPricingIndex] : pricingGeneral[boothPricingIndex];
+		todaysPayment = perBoothPrice * boothQuantity;
 
-        // hide early-bird stuff if date is past
-        if ( ! discount ) {
-            $('.early-bird').hide();
-        }
+		// Set total amount.
+		$('input[name="quantity"]').val(boothQuantity);
+		$('.booth-plural').html(boothQuantity !== 1 ? 's' : '');
+		$('.cost-per-booth').html(perBoothPrice);
+		$('input[name="total-amount"]').val(todaysPayment);
 
-        // check for deposit deadline
-        if ( ! depositDeadline ) {
-            $('input[name="payment-option"][value*="50%"]').parents('.wpcf7-list-item').remove();
-        }
+		// Set today’s payment.
+		if ($('input[name="payment-option"]:checked').val().indexOf('50%') > -1) {
+			$('input[name="todays-payment"]').val((todaysPayment * 0.5).toFixed(2));
+		} else {
+			$('input[name="todays-payment"]').val(todaysPayment.toFixed(2));
+		}
+	}
 
-        // count up number of total booths
-        $('.locations input[name*="quantity"]').each(function(){
-            boothQuantity += Number($(this).val());
-        });
+	$(document).ready(function () {
+		// Set up form.
+		$('.wpcf7-form-control-wrap.locations label[for*="quantity"]').hide();
+		$('input[name="quantity"], input[name="total-amount"], input[name="todays-payment"]').attr('disabled', true);
+		$('span.todays-payment').append('<span class="small"> (automatically calculated)</span>');
 
-        // figure pricing
-        var boothPricingIndex = locationQuantity > 0 ? locationQuantity - 1 : 0,
-            perBoothPrice = discount ? pricingDiscount[boothPricingIndex] : pricingGeneral[boothPricingIndex],
-            todaysPayment = perBoothPrice * boothQuantity;
+		// Show/hide individual booth quantities.
+		$('input[name="locations[]"]').on('change', function () {
+			if ($(this).is(':checked')) {
+				$(this).parents('.wpcf7-list-item').find('label.quantity').show().find('input').val(1);
+			} else {
+				$(this).parents('.wpcf7-list-item').find('label.quantity').hide().find('input').val(0);
+			}
+		});
 
-        // set total amount
-        $('input[name="quantity"]').val(boothQuantity);
-        $('.booth-plural').html(boothQuantity != 1 ? 's' : '');
-        $('.cost-per-booth').html(perBoothPrice);
-        $('input[name="total-amount"]').val(todaysPayment);
+		// Handle pluralization.
+		$('input.quantity-by-convention').on('change', function () {
+			$(this).next('.plural').html($(this).val() !== 1 ? 's' : '');
+		});
 
-        // today's payment
-        if ($('input[name="payment-option"]:checked').val().indexOf('50%') > -1) {
-            $('input[name="todays-payment"]').val((todaysPayment * .5).toFixed(2));
-        } else {
-            $('input[name="todays-payment"]').val(todaysPayment.toFixed(2));
-        }
-    }
+		// Handle changes.
+		$('input.quantity-by-convention, input[name="locations[]"], input[name^="quantity"], input[name="payment-option"]').on('change', updatePaymentInfo);
 
-})(jQuery);
+		// Run main function.
+		updatePaymentInfo();
+
+		// Show/hide payment fields.
+		$('input[name="payment-method"]').on('change', handlePaymentFields($(this)));
+		handlePaymentFields($('input[name="payment-method"]:checked'));
+
+		// Re-enable disabled fields prior to form submission.
+		$('form.wpcf7-form').on('submit', function () {
+			$('input[disabled]').attr('disabled', false);
+		});
+	});
+
+}(jQuery));
